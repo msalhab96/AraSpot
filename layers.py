3b3287740d9f6f69f5ec98e6182bb35afdb30aca
msalhab96 <mahmoud@salhab.work>
2022-11-03 06:27:12 +0200

    Encoder added

--- layers.py
+++ layers.py
@@ -53,3 +53,43 @@ class GRUStack(nn.Module):
         hn = hn.permute(1, 0, 2)
         hn = hn.contiguous().view(hn.shape[0], 1, -1)
         return out, hn
+
+
+class Encoder(nn.Module):
+    def __init__(
+            self,
+            n_mels: int,
+            n_conv: int,
+            kernel_size: int,
+            n_channels: int,
+            n_layers: int,
+            hidden_size: int,
+            bidirectional: bool,
+            enc_p_dropout: float
+            ):
+        super().__init__()
+        self.conv_layers = nn.ModuleList(
+            nn.Conv2d(
+                in_channels=1 if i == 0 else n_channels * i,
+                out_channels=1 if i == n_conv - 1 else n_channels * (1 + i),
+                kernel_size=kernel_size
+            )
+            for i in range(n_conv)
+        )
+        in_size = n_mels - n_conv * (kernel_size - 1)
+        self.grus = GRUStack(
+            n_layers=n_layers,
+            in_size=in_size,
+            hidden_size=hidden_size,
+            bidirectional=bidirectional
+        )
+        self.dropout = nn.Dropout(enc_p_dropout)
+
+    def forward(self, x: Tensor):
+        out = x.unsqueeze(dim=1)
+        for layer in self.conv_layers:
+            out = layer(out)
+        out = out.squeeze(dim=1)
+        out = self.dropout(out)
+        out, hn = self.grus(out)
+        return out, hn
