a532dc06114ffe367dfa0c6e8c1497d002cee45c
msalhab96 <mahmoud@salhab.work>
2022-11-03 06:26:21 +0200

    GRUStack

--- layers.py
+++ layers.py
@@ -24,3 +24,32 @@ class GRUBlock(nn.Module):
         out, hn = self.gru(x)
         out = self.lnorm(out)
         return out, hn
+
+
+class GRUStack(nn.Module):
+    def __init__(
+            self,
+            n_layers: int,
+            in_size: int,
+            hidden_size: int,
+            bidirectional: bool
+            ):
+        super().__init__()
+        self.layers = nn.ModuleList(
+            GRUBlock(
+                in_size=in_size if i == 0 else hidden_size * (
+                    2 if bidirectional else 1
+                    ),
+                hidden_size=hidden_size,
+                bidirectional=bidirectional
+            )
+            for i in range(n_layers)
+        )
+
+    def forward(self, x: Tensor):
+        out = x
+        for layer in self.layers:
+            out, hn = layer(out)
+        hn = hn.permute(1, 0, 2)
+        hn = hn.contiguous().view(hn.shape[0], 1, -1)
+        return out, hn
