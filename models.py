fe0be796f674e8182ee160945903a06a13a87b30
msalhab96 <mahmoud@salhab.work>
2022-11-05 12:03:15 +0300

    ConvGRU replaced by ConformerGRU

--- models.py
+++ models.py
@@ -1,64 +1,62 @@
 import os
 from config import get_model_args
-from layers import Decoder, Encoder
+from layers import Conformer
 from torch import Tensor
 from torch import nn
 import torch
 
 
-class ConvSeq2Seq(nn.Module):
+class ConformerGRU(nn.Module):
     def __init__(
             self,
-            n_mels: int,
-            n_conv: int,
-            kernel_size: int,
-            n_channels: int,
-            voc_size: int,
-            emb_dim: int,
-            n_classes: int,
+            feat_size: int,
             n_layers: int,
-            hidden_size: int,
+            enc_dim: int,
+            h: int,
+            kernel_size: int,
+            scaling_factor: int,
+            residual_scaler: float,
             bidirectional: bool,
-            enc_hidden_size: int,
-            dec_hidden_size: int,
-            dec_p_dropout: float,
-            enc_p_dropout: float
-            ):
+            n_classes: int,
+            device: str,
+            p_dropout: float
+            ) -> None:
         super().__init__()
-        self.enc = Encoder(
-            n_mels=n_mels,
-            n_conv=n_conv,
-            kernel_size=kernel_size,
-            n_channels=n_channels,
+        self.conf = Conformer(
             n_layers=n_layers,
-            hidden_size=hidden_size,
-            bidirectional=bidirectional,
-            enc_p_dropout=enc_p_dropout
+            enc_dim=enc_dim,
+            h=h,
+            kernel_size=kernel_size,
+            scaling_factor=scaling_factor,
+            residual_scaler=residual_scaler,
+            device=device,
+            p_dropout=p_dropout
         )
-        dsize = enc_hidden_size * 2
-        self.dec = Decoder(
-            vocab_size=voc_size,
-            emb_dim=emb_dim,
-            enc_hidden_size=dsize if bidirectional else enc_hidden_size,
-            dec_hidden_size=dec_hidden_size,
-            p_dropout=dec_p_dropout
+        self.gru = nn.GRU(
+            input_size=enc_dim,
+            hidden_size=enc_dim,
+            batch_first=True,
+            bidirectional=bidirectional
         )
         self.pred_fc = nn.Linear(
-            in_features=dsize if bidirectional else enc_hidden_size,
+            in_features=2 * enc_dim if bidirectional else enc_dim,
             out_features=n_classes
         )
+        self.fc = nn.Linear(
+            in_features=feat_size, out_features=enc_dim
+            )
 
-    def forward(self, x: Tensor, y: Tensor):
-        out, hn = self.enc(x)
-        result = self.dec(y, hn)
-        hn = hn.squeeze(dim=1)
-        preds = self.pred_fc(hn)
-        return out, preds, result
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.fc(x)
+        out = self.conf(out)
+        out, h = self.gru(out)
+        h = h.permute(1, 0, 2).view(x.shape[0], -1)
+        return self.pred_fc(h)
 
 
-def get_model(cfg, vocab_size, n_classes):
-    model = ConvSeq2Seq(
-        **get_model_args(cfg, vocab_size, n_classes)
+def get_model(cfg, n_classes):
+    model = ConformerGRU(
+        **get_model_args(cfg, n_classes)
     )
     if os.path.exists(cfg.ckpt_path) is True:
         model.load_state_dict(torch.load(cfg.ckpt_path))
