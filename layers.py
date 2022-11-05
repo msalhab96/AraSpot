031063bbe833d03cbff432091f551fbe7ad9e46b
msalhab96 <mahmoud@salhab.work>
2022-11-05 11:29:26 +0300

    MHSA added

--- layers.py
+++ layers.py
@@ -1,5 +1,9 @@
+import math
+import torch
 import torch.nn as nn
+from typing import List
 from torch import Tensor
+from functools import lru_cache
 
 
 class FeedForwardModule(nn.Module):
@@ -123,3 +127,155 @@ class ConvModule(nn.Module):
         out = self.dropout(out)
         out = out.permute(0, 2, 1)
         return out + inp
+
+
+class MHSA(nn.Module):
+    def __init__(
+            self,
+            enc_dim: int,
+            h: int,
+            p_dropout: float,
+            device: str
+            ) -> None:
+        super().__init__()
+        assert enc_dim % h == 0, 'enc_dim is not divisible by h'
+        self.fc_key = nn.Linear(
+            in_features=enc_dim,
+            out_features=enc_dim,
+        )
+        self.fc_query = nn.Linear(
+            in_features=enc_dim,
+            out_features=enc_dim,
+        )
+        self.fc_value = nn.Linear(
+            in_features=enc_dim,
+            out_features=enc_dim,
+        )
+        self.proj_fc = nn.Linear(
+            in_features=2 * enc_dim,
+            out_features=enc_dim,
+        )
+        self.lnorm = nn.LayerNorm(enc_dim)
+        self.dropout = nn.Dropout(p_dropout)
+        self.enc_dim = enc_dim
+        self.h = h
+        self.dk = enc_dim // h
+        self.sqrt_dk = math.sqrt(self.dk)
+        self.softmax = nn.Softmax(dim=-1)
+        self.device = device
+
+    def _get_scaled_att(
+            self,
+            Q: Tensor,
+            K: Tensor
+            ) -> Tensor:
+        """Calculates the scaled attention map
+        by calculating softmax(matmul(Q, K.T)/sqrt(dk))
+        Args:
+            Q (Tensor): The Query tensor of shape [h * B, Tq, dk]
+            K (Tensor): The Key tensor of shape [h * B, dk, Tk]
+        Returns:
+            Tensor: The scaled attention weights of shape
+            [B * h, Tq, Tk]
+        """
+        result = torch.matmul(Q, K)
+        result = result / self.sqrt_dk
+        return self.softmax(result)
+
+    def perform_att(
+            self,
+            Q: Tensor,
+            K: Tensor,
+            V: Tensor
+            ) -> Tensor:
+        """Performs multi-head scaled attention
+        by calculating softmax(matmul(Q, K.T)/sqrt(dk)).V
+        Args:
+            Q (Tensor): The Query tensor of shape [h * B, Tq, dk]
+            K (Tensor): The Key tensor of shape [h * B, dk, Tk]
+            V (Tensor): The Value tensor of shape [h * B, Tk, dk]
+        Returns:
+            Tuple[Tensor, Tensor]: The attention matrix of shape
+            [B * h, Tq, Tk] and the scaled attention value of
+            shape [B * h, Tq, dk].
+        """
+        att = self._get_scaled_att(Q, K)
+        result = torch.matmul(att, V)
+        return att, result
+
+    @lru_cache(maxsize=2)
+    def get_positionals(self, max_length: int) -> Tensor:
+        """Create Positionals tensor to be added to the input
+        Args:
+            max_length (int): The maximum length of the positionals sequence.
+        Returns:
+            Tensor: Positional tensor
+        """
+        result = torch.zeros(max_length, self.enc_dim, dtype=torch.float)
+        for pos in range(max_length):
+            for i in range(0, self.enc_dim, 2):
+                denominator = pow(10000, 2 * i / self.enc_dim)
+                result[pos, i] = math.sin(pos / denominator)
+                result[pos, i + 1] = math.cos(pos / denominator)
+        return result
+
+    def _reshape(self, *args) -> List[Tensor]:
+        """Reshabes all the given list of tensor
+        from [B, T, N] to [B, T, h, dk]
+        Returns:
+            List[Tensor]: list of all reshaped tensors
+        """
+        return [
+            item.contiguous().view(-1, item.shape[1], self.h, self.dk)
+            for item in args
+        ]
+
+    def _pre_permute(self, *args) -> List[Tensor]:
+        """Permutes all the given list of tensors
+        from [B, T, h, dk] to become [h, B, T, dk].
+        Returns:
+            List[Tensor]: List of all permuted tensors.
+        """
+        return [
+            item.permute(2, 0, 1, 3)
+            for item in args
+        ]
+
+    def _change_dim(self, *args) -> List[Tensor]:
+        """Changes the dimensionality of all passed tensores
+        from [B, T, N] to [B * h, T, dk]
+        Returns:
+            List[Tensor]: List of the modified tensors.
+        """
+        result = self._reshape(*args)  # [B, T, h, dk]
+        result = self._pre_permute(*result)  # [h, B, T, dk]
+        return [
+            item.permute(1, 0, 2, 3).contiguous().view(
+                -1, item.shape[2], item.shape[3]
+                )
+            for item in result
+        ]
+
+    def forward(self, inp: Tensor) -> Tensor:
+        """Passes the input into multi-head attention
+        Args:
+            inp (Tensor): The input tensor
+        Returns:
+            Tensor: The result after adding it to positionals
+            and passing it through multi-head self-attention
+        """
+        out = self.lnorm(inp)
+        [b, s, _] = inp.shape
+        K = self.fc_key(inp)
+        Q = self.fc_query(inp)
+        V = self.fc_value(inp)
+        (Q, K, V) = self._change_dim(Q, K, V)  # [h * B, T, dk]
+        K = K.permute(0, 2, 1)  # [h, T, B, dk]
+        _, result = self.perform_att(Q, K, V)
+        result = result.view(b, self.h, s, self.dk)
+        result = result.permute(0, 2, 1, 3)
+        result = result.contiguous().view(b, s, -1)
+        result = torch.cat([inp, result], dim=-1)
+        result = self.proj_fc(result)
+        out = self.dropout(result)
+        return inp + out
