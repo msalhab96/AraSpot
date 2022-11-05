a36c3cb7b3ae162cc63e4d0393b8f680b1dd72d0
msalhab96 <mahmoud@salhab.work>
2022-11-05 11:26:32 +0300

    FeedForwardModule added, all layers would be replaced by Conformer

--- layers.py
+++ layers.py
@@ -1,144 +1,59 @@
-import torch
-from torch import nn
+import torch.nn as nn
 from torch import Tensor
 
 
-class GRUBlock(nn.Module):
+class FeedForwardModule(nn.Module):
+    """Implements the feed forward module in the conformer block
+    where the module consists of the below
+    1. Layer Norm
+    2. Linear Layer
+    3. Swish Activation
+    4. Dropout
+    5. Linear Layer
+    6. Dropout
+    Args:
+        enc_dim (int): The encoder dimensionality
+        scaling_factor (int): The scaling factor of the linear layer
+        p_dropout (float): The dropout probability
+        residual_scaler (float, optional): The residual scaling.
+        Defaults to 0.5.
+    """
     def __init__(
             self,
-            in_size: int,
-            hidden_size: int,
-            bidirectional: bool
-            ):
+            enc_dim: int,
+            scaling_factor: int,
+            p_dropout: float,
+            residual_scaler=0.5
+            ) -> None:
         super().__init__()
-        self.gru = nn.GRU(
-            input_size=in_size,
-            hidden_size=hidden_size,
-            batch_first=True,
-            bidirectional=bidirectional
+        self.residual_scaler = residual_scaler
+        scaled_dim = scaling_factor * enc_dim
+        self.lnorm = nn.LayerNorm(enc_dim)
+        self.fc1 = nn.Linear(
+            in_features=enc_dim,
+            out_features=scaled_dim
         )
-        self.lnorm = nn.LayerNorm(
-            normalized_shape=2*hidden_size if bidirectional else hidden_size
-            )
-
-    def forward(self, x: Tensor):
-        out, hn = self.gru(x)
-        out = self.lnorm(out)
-        return out, hn
-
-
-class GRUStack(nn.Module):
-    def __init__(
-            self,
-            n_layers: int,
-            in_size: int,
-            hidden_size: int,
-            bidirectional: bool
-            ):
-        super().__init__()
-        self.layers = nn.ModuleList(
-            GRUBlock(
-                in_size=in_size if i == 0 else hidden_size * (
-                    2 if bidirectional else 1
-                    ),
-                hidden_size=hidden_size,
-                bidirectional=bidirectional
-            )
-            for i in range(n_layers)
+        self.fc2 = nn.Linear(
+            in_features=scaled_dim,
+            out_features=enc_dim
         )
-
-    def forward(self, x: Tensor):
-        out = x
-        for layer in self.layers:
-            out, hn = layer(out)
-        hn = hn.permute(1, 0, 2)
-        hn = hn.contiguous().view(hn.shape[0], 1, -1)
-        return out, hn
-
-
-class Encoder(nn.Module):
-    def __init__(
-            self,
-            n_mels: int,
-            n_conv: int,
-            kernel_size: int,
-            n_channels: int,
-            n_layers: int,
-            hidden_size: int,
-            bidirectional: bool,
-            enc_p_dropout: float
-            ):
-        super().__init__()
-        self.conv_layers = nn.ModuleList(
-            nn.Conv2d(
-                in_channels=1 if i == 0 else n_channels * i,
-                out_channels=1 if i == n_conv - 1 else n_channels * (1 + i),
-                kernel_size=kernel_size
-            )
-            for i in range(n_conv)
-        )
-        in_size = n_mels - n_conv * (kernel_size - 1)
-        self.grus = GRUStack(
-            n_layers=n_layers,
-            in_size=in_size,
-            hidden_size=hidden_size,
-            bidirectional=bidirectional
-        )
-        self.dropout = nn.Dropout(enc_p_dropout)
-
-    def forward(self, x: Tensor):
-        out = x.unsqueeze(dim=1)
-        for layer in self.conv_layers:
-            out = layer(out)
-        out = out.squeeze(dim=1)
-        out = self.dropout(out)
-        out, hn = self.grus(out)
-        return out, hn
-
-
-class Decoder(nn.Module):
-    def __init__(
-            self,
-            vocab_size: int,
-            emb_dim: int,
-            enc_hidden_size: int,
-            dec_hidden_size: int,
-            p_dropout=0.25
-            ):
-        super().__init__()
-        self.emb = nn.Embedding(
-            num_embeddings=vocab_size,
-            embedding_dim=emb_dim
-        )
-        self.gru = nn.GRU(
-            input_size=emb_dim,
-            hidden_size=dec_hidden_size,
-            batch_first=True,
-            bidirectional=False
-            )
-        self.fc = nn.Linear(
-            in_features=enc_hidden_size,
-            out_features=dec_hidden_size
-        )
-        self.pred = nn.Linear(
-            in_features=dec_hidden_size,
-            out_features=vocab_size
-        )
+        self.swish = nn.SiLU()
         self.dropout = nn.Dropout(p_dropout)
 
-    def forward(self, y: Tensor, hn: Tensor):
-        hn = self.fc(hn)
-        hn = hn.permute(1, 0, 2)
-        max_len = y.shape[1]
-        y = self.emb(y)
-        y = self.dropout(y)
-        results = None
-        for i in range(max_len):
-            temp = y[:, i:i + 1, :]
-            out, hn = self.gru(temp, hn)
-            if results is None:
-                results = out
-            else:
-                results = torch.cat([results, out], dim=1)
-        results = self.pred(results)
-        return results
+    def forward(self, inp: Tensor) -> Tensor:
+        """Passes the given inp through the feed forward
+        module
+        Args:
+            inp (Tensor): the input to the feed forward module
+            with shape [B, M, N] where B is the batch size, M
+            is the maximum length, and N is the encoder dim
+        Returns:
+            Tensor: The result of the forward module
+        """
+        out = self.lnorm(inp)
+        out = self.fc1(out)
+        out = self.swish(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
+        out = self.dropout(out)
+        return self.residual_scaler * inp + out
