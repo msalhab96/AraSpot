c13fb9c64931a5a2bb00cd827cd3b4be70686077
msalhab96 <mahmoud@salhab.work>
2022-11-06 00:07:28 +0300

    loaders factory added, unnecessary vars removed

--- data.py
+++ data.py
@@ -3,8 +3,9 @@ from torch import Tensor
 from pathlib import Path
 from typing import Union, Tuple
 from interfaces import IProcessor
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from constants import path_column, class_column
+from processors import get_augmenter, get_speech_processor
 
 
 class Data(Dataset):
@@ -12,13 +13,11 @@ class Data(Dataset):
             self,
             data_path: Union[str, Path],
             file_processor: IProcessor,
-            text_processor: IProcessor,
             cls_mapper: dict
             ) -> None:
         super().__init__()
         self.df = pd.read_csv(data_path)
         self.file_processor = file_processor
-        self.text_processor = text_processor
         self.cls_mapper = cls_mapper
 
     def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor, int]:
@@ -26,9 +25,30 @@ class Data(Dataset):
         file = row[path_column]
         text = row[class_column]
         cls = self.cls_mapper[text]
-        text, mask = self.text_processor.process(text)
-        speech = self.text_processor.process(file)
-        return speech, text, mask, cls
+        speech = self.file_processor.process(file)
+        return speech, cls
 
     def __len__(self):
         return self.df.shape[0]
+
+
+def get_loaders(cfg, cls_mapper: dict):
+    train_data = Data(
+        data_path=cfg.train_path,
+        file_processor=get_speech_processor(
+            cfg, augment=True, augmenter=get_augmenter(cfg)
+            ),
+        cls_mapper=cls_mapper
+    )
+    test_data = Data(
+        data_path=cfg.test_path,
+        file_processor=get_speech_processor(cfg, augment=False),
+        cls_mapper=cls_mapper
+    )
+    train_loader = DataLoader(
+        train_data, batch_size=cfg.batch_size, shuffle=True
+        )
+    test_loader = DataLoader(
+        test_data, batch_size=cfg.batch_size, shuffle=True
+        )
+    return train_loader, test_loader
