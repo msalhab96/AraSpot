dfbebd37bd8c36de66db4c31f001d44283b77aaa
msalhab96 <mahmoud@salhab.work>
2022-11-06 16:56:15 +0300

    load_json added, tensor converted to float for mean

--- utils.py
+++ utils.py
@@ -1,4 +1,13 @@
+import json
+
+
 def calc_acc(preds, target):
     preds = preds.view(-1)
     target = target.view(-1)
-    return (preds == target).mean()
+    return (preds == target).float().mean()
+
+
+def load_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
