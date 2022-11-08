04aeb01197a5fd57f69f92ebe5c92eb1a7895409
msalhab96 <mahmoud@salhab.work>
2022-11-08 17:46:11 +0300

    train.py updated

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
+import math
 import os
 import torch
-from utils import calc_acc
+from utils import calc_acc, load_json
 
 
 class Trainer:
@@ -25,77 +26,82 @@ class Trainer:
         self.epochs = epochs
         self.device = device
         self.logger = logger
+        self._loss = math.inf
+        self._last = math.inf
+
+    def is_terminate(self):
+        # TODO
+        return False
 
     def fit(self):
         for epoch in range(self.epochs):
             self.train()
             self.test()
             self.logger.log()
-            if self.is_terminate() is True:
-                print('The model is not improving any more!')
-                break
-            else:
+            if self._last < self._loss:
+                self._loss = self._last
                 self.export_ckpt(epoch)
 
     def export_ckpt(self, epoch: int):
         path = os.path.join(self.outdir, f'checkpoint_{epoch}.pt')
+        print(f'checkpoint {path} saved!')
+        print('=' * 50)
         torch.save(self.model.state_dict(), path)
 
     def train(self):
         self.model.train()
         total_loss = 0
-        preds = []
-        targets = []
+        all_preds = []
+        all_targets = []
         for batch in self.train_loader:
-            [speech, text, mask, cls] = batch
+            [speech, cls] = batch
             speech = speech.to(self.device)
-            text = text.to(self.device)
-            mask = mask.to(self.device)
             cls = cls.to(self.device)
             self.optimizer.zero_grad()
-            out, preds, result = self.model(speech, text)
-            loss = self.criterion(
-                out, preds, result, text, mask, cls
-                )
+            preds = self.model(speech)
+            loss = self.criterion(preds, cls)
             loss.backward()
             self.optimizer.step()
-            preds.append(torch.argmax(preds.cpu(), dim=-1))
-            targets.append(cls.cpu())
             total_loss += loss.item()
         self.logger.record(
             'train_loss', total_loss / len(self.train_loader)
             )
+        for batch in self.train_loader:
+            [speech, cls] = batch
+            speech = speech.to(self.device)
+            cls = cls.to(self.device)
+            preds = self.model(speech)
+            all_preds.append(torch.argmax(preds.cpu(), dim=-1))
+            all_targets.append(cls.cpu())
         self.logger.record(
             'train_acc', calc_acc(
-                torch.vstack(preds),
-                torch.vstack(targets)
+                torch.hstack(all_preds),
+                torch.hstack(all_targets)
                 )
             )
 
+    @torch.no_grad()
     def test(self):
         self.model.eval()
         total_loss = 0
-        preds = []
-        targets = []
+        all_preds = []
+        all_targets = []
         for batch in self.test_loader:
-            [speech, text, mask, cls] = batch
+            [speech, cls] = batch
             speech = speech.to(self.device)
-            text = text.to(self.device)
-            mask = mask.to(self.device)
             cls = cls.to(self.device)
-            out, preds, result = self.model(speech, text)
-            loss = self.criterion(
-                out, preds, result, text, mask, cls
-                )
-            preds.append(torch.argmax(preds.cpu(), dim=-1))
-            targets.append(cls.cpu())
+            preds = self.model(speech)
+            loss = self.criterion(preds, cls)
+            all_preds.append(torch.argmax(preds.cpu(), dim=-1))
+            all_targets.append(cls.cpu())
             total_loss += loss.item()
         self.logger.record(
             'test_loss', total_loss / len(self.train_loader)
             )
         self.logger.record(
             'test_acc', calc_acc(
-                torch.vstack(preds),
-                torch.vstack(targets)
+                torch.hstack(all_preds),
+                torch.hstack(all_targets)
             )
         )
+        self._last = total_loss
